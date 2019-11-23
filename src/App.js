d3a639bf80bd8f91e1dd0c304447194cd7254ba1
Aman Chermashev <bakytaman228337@gmail.com>
2019-11-23 17:15:34 +0600

    #2 Made Add and Remove Functions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,43 @@ import './App.css';
 import Menu from "./Components/Menu/Menu";
 
 class App extends Component {
+  state = {
+    goods:[
+      {name: 'Hamburger', count: 0},
+      {name: 'Cheeseburger', count: 0},
+      {name: 'Fries', count: 0},
+      {name: 'Coffee', count: 0},
+      {name: 'Tea', count: 0},
+      {name: 'Cola', count: 0},
+    ],
+    totalPrice: 0,
+  };
+
+  addGood = good => {
+    const goods = [...this.state.goods];
+    const goodName = this.state.goods.findIndex(i => i.name === good.name);
+    goods[goodName].count++;
+    let totalPrice = this.state.totalPrice;
+    totalPrice += good.price;
+    let goodPrice = good.price * goods[goodName].count;
+    this.setState({goods, totalPrice});
+    console.log(goods[goodName].count, good.name, goodPrice);
+  };
+
+  removeGood = good => {
+    const goods = [...this.state.goods];
+    const goodName = this.state.goods.findIndex(i => i.name === good.name);
+    goods[goodName].count--;
+    let totalPrice = this.state.totalPrice;
+    totalPrice -= good.price;
+    let goodPrice = good.price * goods[goodName].count;
+    this.setState({goods, totalPrice});
+    console.log(goods[goodName].count, good.name, goodPrice);
+  };
   render() {
     return (
       <div className="App">
-       <Menu/>
+        <Menu addGood={this.addGood}/>
       </div>
     );
   }
